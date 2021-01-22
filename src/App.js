0f18106dfaf4c390c3fcb977a9e2dc260f566410
blackdevil30 <nithinnj007@gmail.com>
2021-01-22 22:20:19 +0530

    added signIn page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from './pages/home/homepage.component';
 import {Route,Switch} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SigninAndSignupPage from './pages/signin-signup-page/signin-signup-page.component';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route  path='/shop' component={ShopPage}/>
+          <Route  path='/signin' component={SigninAndSignupPage}/>
       </Switch>
      
     </div>
